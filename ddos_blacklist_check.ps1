826644721f5994f79da50aa1011cc743b8c407b6
matticamp <2312425+matticamp@users.noreply.github.com>
2018-06-14 11:07:19 -0400

    Update ddos_blacklist_check.ps1

--- ddos_blacklist_check.ps1
+++ ddos_blacklist_check.ps1
@@ -3,7 +3,8 @@ recently used for DDos attacks, published on peerlyst.com
 
 Created by Mattia Campagnano on Thu Jun 14 10:26:25 2018 EDT
 
-The list is available on Peerlyst at the following URL: https://www.peerlyst.com/posts/attention-a-new-kind-of-ddos-mark-sitkowski?trk=explore_page_posts_featured_feed_entry
+The list is available on Peerlyst at the following URL: 
+https://www.peerlyst.com/posts/attention-a-new-kind-of-ddos-mark-sitkowski?trk=explore_page_posts_featured_feed_entry
 
 To obtain only the IP addresses, if you have Bash (Windows Subsystem for Linux) installed in Windows 10,
 you can use the following command example: 
